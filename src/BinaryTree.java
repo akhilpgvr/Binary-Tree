e086835c8390d2cb9ec2f7dcd24ea2c251307c36
Akhil <akhilpgvr@gmail.com>
2024-07-21 14:39:39 +0530

    inorder traversal, preorder traversal and post order traversal -- completed

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -15,6 +15,8 @@ public class BinaryTree {
             root = newChild;
             return;
         }
+
+        //queue for tracking the nodes
         Queue<Node> queue = new LinkedList<>();
         queue.offer(root);
         while(!queue.isEmpty()) {
@@ -32,5 +34,36 @@ public class BinaryTree {
         }
     }
 
+    //Performing Inorder traversal in binary tree
+    public void inorderTraversal(Node root) {
 
+        if(root == null) return;
+        else{
+            inorderTraversal(root.leftChild);
+            System.out.print(" "+ root.data+ " ");
+            inorderTraversal(root.rightChild);
+        }
+    }
+
+    //Performing Preorder traversal in binary tree
+    public void preorderTraversal(Node root) {
+
+        if(root == null) return;
+        else{
+            System.out.print(" "+ root.data+ " ");
+            preorderTraversal(root.leftChild);
+            preorderTraversal(root.rightChild);
+        }
+    }
+
+    //Performing Postorder traversal in binary tree
+    public void postorderTraversal(Node root) {
+
+        if(root == null) return;
+        else{
+            postorderTraversal(root.leftChild);
+            postorderTraversal(root.rightChild);
+            System.out.print(" "+ root.data+ " ");
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,33 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("...............................");
+        System.out.println("creating binary tree");
+        System.out.println("...............................");
         BinaryTree binaryTree = new BinaryTree();
+        binaryTree.addChild(1);
         binaryTree.addChild(2);
         binaryTree.addChild(3);
         binaryTree.addChild(4);
+        binaryTree.addChild(5);
+        binaryTree.addChild(6);
+        binaryTree.addChild(7);
+        binaryTree.addChild(8);
+        binaryTree.addChild(9);
         System.out.println("...............................");
+        System.out.println("Inorder Traversal on binary tree");
+        System.out.println("...............................");
+        binaryTree.inorderTraversal(binaryTree.root);
+        System.out.println();
+        System.out.println("...............................");
+        System.out.println("Preorder Traversal on binary tree");
+        System.out.println("...............................");
+        binaryTree.preorderTraversal(binaryTree.root);
+        System.out.println();
+        System.out.println("...............................");
+        System.out.println("Postorder Traversal on binary tree");
+        System.out.println("...............................");
+        binaryTree.postorderTraversal(binaryTree.root);
+        System.out.println();
         System.out.println("...............................");
         System.out.println("...............................");
     }
